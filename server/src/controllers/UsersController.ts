f6c97fad7092a87ba32ffbf26bae2a4d279c8197
João Vinaud <leyenasd@gmail.com>
2020-08-15 10:43:08 -0300

    Chore: Create endpoint for new user

--- server/src/controllers/UsersController.ts
+++ server/src/controllers/UsersController.ts
@@ -2,11 +2,6 @@ import {Request, Response} from 'express';
 import db from '../database/connection';
 
 
-interface ScheduleItem{
-    week_day: number;
-    from: string;
-    to: string;
-}
 
 export default class UsersController{
     async index(request: Request, response: Response) {
@@ -16,6 +11,54 @@ export default class UsersController{
 
     async create(request: Request, response: Response) {
         
-        return response.json({message: "hello new user"});
-    };
+        const {
+            name, 
+            age,
+            gender,
+            latitude,
+            longitude,
+            water,
+            weapon,
+            medicine,
+            food
+           
+        } = request.body;
+
+        const infected = false;
+        const reports = 0;
+    
+        const transaction = await db.transaction();
+    
+        try {
+            const insertedUsersIds = await transaction('users').insert({
+                name,
+                age,
+                gender,
+                latitude,
+                longitude,
+                infected,
+                reports
+            });
+        
+            const user_id = insertedUsersIds[0];
+            
+            const insertedInventoryIds = await transaction('inventory').insert({
+                water,
+                weapon,
+                medicine,
+                food,
+                user_id
+            });
+
+            await transaction.commit();
+        
+            return response.status(201).send();
+        } catch (err) {
+            await transaction.rollback();
+            return response.status(400).json({
+                error: 'Erro ao cadastrar o usuário no sistema'
+            })
+        }
+    
+    }
 }
