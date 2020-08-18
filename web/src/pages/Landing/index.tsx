48c52c667da121e25995a16dbf0f83180470b709
João Vinaud <leyenasd@gmail.com>
2020-08-18 12:50:42 -0300

    Chore: Add Routes configuration

---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,11 @@
+import React,  { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
+
+function Landing() {
+
+return(
+    <div><h1>Hello Warudo!</h1></div>
+)
+}
+
+export default Landing;
